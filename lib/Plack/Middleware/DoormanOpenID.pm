24f672d8e2a793642062710aa7721c4707d191bc
劉康民 <gugod@gugod.org>
2011-03-31 15:21:35 +0000

    Import of GUGOD/Doorman-0.02 from CPAN.
    gitpan-cpan-distribution: Doorman
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         GUGOD/Doorman-0.02.tar.gz
    gitpan-cpan-author:       GUGOD
    gitpan-cpan-maturity:     released

--- lib/Plack/Middleware/DoormanOpenID.pm
+++ lib/Plack/Middleware/DoormanOpenID.pm
@@ -1,16 +1,33 @@
 package Plack::Middleware::DoormanOpenID;
 use 5.010;
 use strict;
+use Doorman;
+
+our $VERSION   = $Doorman::VERSION;
+our $AUTHORITY = $Doorman::AUTHORITY;
+
 use feature qw(switch);
 use parent 'Doorman::PlackMiddleware';
 
 use Plack::Request;
-use Plack::Util::Accessor qw(root_url scope);
+use Plack::Util::Accessor qw(root_url scope secret ua);
 
 use Net::OpenID::Consumer;
 use LWPx::ParanoidAgent;
 use URI;
 use Scalar::Util qw(weaken);
+use Plack::Session;
+
+sub prepare_app {
+    my $self = shift;
+
+    $self->secret(
+        'This is the default consumer_secret value for Net::OpenID::Consumer
+         that you should provide for your own app. ' . $VERSION
+    ) unless $self->secret;
+    $self->ua('LWPx::ParanoidAgent') unless $self->ua;
+    $self->scope('users') unless $self->scope;
+}
 
 sub openid_verified_uri {
     my ($self) = @_;
@@ -26,12 +43,9 @@ sub verified_identity_url {
     my ($self) = @_;
     my $env = $self->{env};
     my $scope = $self->scope;
-    my $session = $env->{'psgix.session'};
+    my $session = Plack::Session->new($env);
 
-    if ($session && $session->{"doorman.${scope}.openid.verified_identity_url"}) {
-        return $session->{"doorman.${scope}.openid.verified_identity_url"};
-    }
-    return;
+    return $session->get("doorman.${scope}.openid.verified_identity_url");
 }
 
 sub is_sign_in {
@@ -41,17 +55,24 @@ sub is_sign_in {
 
 sub csr {
     my ($self, $request) = @_;
+
+    if (!$self->root_url) {
+        my $root_uri = $request->uri;
+        $root_uri->path("/");
+        $self->root_url($root_uri->as_string);
+    }
+
     return Net::OpenID::Consumer->new(
-        ua => LWPx::ParanoidAgent->new,
+        ua => ref($self->ua) ? $self->ua : $self->ua->new,
         args => sub { $request->param($_[0]) },
-        consumer_secret => "lipsum",
+        consumer_secret => $self->secret,
         required_root   => $self->root_url
     );
 }
 
 sub call {
     my ($self, $env) = @_;
-    my $session = $env->{'psgix.session'};
+    my $session = Plack::Session->new($env);
     die "Session is required for Doorman.\n" unless $session;
 
     $env->{"doorman.@{[ $self->scope ]}.openid"} = $self;
@@ -81,6 +102,7 @@ sub call {
 
         when(['GET', $self->openid_verified_path]) {
             my $csr = $self->csr($request);
+
             $csr->handle_server_response(
                 verified => sub {
                     my $id = shift;
@@ -88,7 +110,7 @@ sub call {
                     $env->{'doorman.'. $self->scope .'.openid.verified_identity'} = $id;
                     $env->{'doorman.'. $self->scope .'.openid.status'} = 'verified';
 
-                    $session->{'doorman.'. $self->scope .'.openid.verified_identity_url'} = $id->url;
+                    $session->set('doorman.'. $self->scope .'.openid.verified_identity_url', $id->url);
                 },
                 setup_required => sub {
                     $env->{'doorman.'. $self->scope .'.openid.status'} = 'setup_required';
@@ -108,7 +130,7 @@ sub call {
         }
 
         when(['GET', $self->sign_out_path]) {
-            delete $session->{'doorman.'. $self->scope .'.openid.verified_identity_url'};
+            $session->remove('doorman.'. $self->scope .'.openid.verified_identity_url');
         }
     }
 
@@ -128,7 +150,7 @@ Plack::Middleware::DoormanOpenID - The OpenID sign-in middleware.
     use Plack::Builder;
     builder {
         enable "Session::Cookie";
-        enable "DoormanOpenID", root_url => 'http://localhost:5000', scope => 'users';
+        enable "DoormanOpenID", scope => 'users';
 
         sub {
             my $env = shift;
@@ -139,7 +161,58 @@ Plack::Middleware::DoormanOpenID - The OpenID sign-in middleware.
 
 =head1 DESCRIPTION
 
-=he1d METHODS
+=head1 OPTIONS
+
+=over 4
+
+=item * secret
+
+YOU MUST PROVIDE THIS VALUE IN YOUR PRODUCTION APP.
+
+The consumer secret string to initiate the Net::OpenID::Consumer object. It should be a long, random,
+difficult-to-guess string. For example:
+
+    T{"<gshFg$Xi<]|r%io\%7MS]'Foj=)2YKiGeB<6FFePPS*h}%meU?H]0/Pu,x/QX.Vq4\Pljr=)yjcI]/M(EFft~_)'$wsIEZuCbc=uWpj-5Fkp>GZl~|/_-4Qk`+4F&V8cg%{/a\-<
+
+DoormanOpenID provides some default value for you to quickly play with
+Doorman without having to provide too many configs. However, if you do
+not provide your own value, malicious attackers might be able forge
+your app in a man-in-middle attacking scenario.
+
+=item * scope
+
+This setting is optional with default value "users", and useful if you
+need multiple roles to login in to your system.
+
+For example, if you need "users" and "admins" roles to have different login session,
+you can achieve it by:
+
+    enable "DoormanOpenID", scope => "users";
+    enable "DoormanOpenID", scope => "admins";
+
+For each scope, a path named after that scope is taken by DoormanOpenID middleware
+as the end-points to perforam openid login.
+
+By default, the following paths and HTTP methods are responded by the
+DoormanOpenID middleware:
+
+    POST /users/sign_in
+    GET  /users/sign_out
+    GET  /users/openid_verified
+
+For the "admins" scope, it'll add:
+
+    POST /admins/sign_in
+    GET  /admins/sign_out
+    GET  /admins/openid_verified
+
+=item * root_url
+
+The application root url that consumes openid. Usually this is guessed, and good enough.
+If your application lives under some path, like, http://foo.com/app, you need to pass that
+as the value of this.
+
+=head1 METHODS
 
 =over 4
 
